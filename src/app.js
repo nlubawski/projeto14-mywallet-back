9d911357ff89b08f94acc71b95b612c94cf27f6f
Nathan Lubawski <nathanlubawski1@gmail.com>
2023-01-21 15:10:30 -0300

    feat: create get /api/statement

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, { application } from 'express'
 import {MongoClient} from 'mongodb'
 import dotenv from 'dotenv'
 import cors from 'cors'
@@ -73,7 +73,26 @@ app.post('/api/sign-in', async(req, res) => {
   }
 })
 
+app.get('/api/statement', async(req,res) => {
+  const {authorization} = req.headers;
+  const token = authorization?.replace('Bearer','').trim()
 
+  if(!token) return res.status(401).send('No session')
+  try {
+      const session = await db.collection('sessions').findOne({token})
+      if(!session) return res.status(401).send('No active session')
+
+      const user = await db.collection('users').findOne({_id:session.userId})
+      if(!user) return res.status(401).send('No user')
+
+      const statement = await db.collection('statements').find({userId: user._id}).toArray()
+      console.log("statement aqui")
+      return res.send(statement)
+
+  } catch (error) {
+    return res.sendStatus(500)
+  }
+})
 
 const port = 5000;
 app.listen(port, () => {
