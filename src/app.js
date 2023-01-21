65c7a92629d11382841d40ab635aae733a1576f9
Nathan Lubawski <nathanlubawski1@gmail.com>
2023-01-21 15:21:28 -0300

    feat: create post /api/statement

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 import express, { application } from 'express'
-import {MongoClient} from 'mongodb'
+import { MongoClient } from 'mongodb'
 import dotenv from 'dotenv'
 import cors from 'cors'
 import joi from 'joi'
 import dayjs from 'dayjs'
 import bcrypt from 'bcrypt'
-import {v4 as uuid} from 'uuid'
+import { v4 as uuid } from 'uuid'
 
 dotenv.config()
 const mongoClient = new MongoClient(process.env.DATABASE_URL)
@@ -23,16 +23,16 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.post('/api/sing-up', async(req,res) => {
-  const {name, email, password, confirmPassord} = req.body
+app.post('/api/sing-up', async (req, res) => {
+  const { name, email, password, confirmPassord } = req.body
   try {
-    const user = await db.collection('users').findOne({email})
-    if(user){
+    const user = await db.collection('users').findOne({ email })
+    if (user) {
       alert('email já cadastrado')
       return res.sendStatus(400)
     }
     const SALT = 10
-    const hash = bcrypt.hashSync(password,SALT)
+    const hash = bcrypt.hashSync(password, SALT)
     await db.collection('users').insertOne({
       name,
       email,
@@ -46,26 +46,26 @@ app.post('/api/sing-up', async(req,res) => {
   }
 })
 
-app.post('/api/sign-in', async(req, res) => {
-  const {email, password} = req.body
-  console.log("email",email)
-  console.log("password",password)
+app.post('/api/sign-in', async (req, res) => {
+  const { email, password } = req.body
+  console.log("email", email)
+  console.log("password", password)
 
   try {
-    const user = await db.collection('users').findOne({email})
-    console.log("user",user)
-    if(!user) return res.sendStatus(404)
-      console.log("cheguei aqui")
+    const user = await db.collection('users').findOne({ email })
+    console.log("user", user)
+    if (!user) return res.sendStatus(404)
+    console.log("cheguei aqui")
 
-    if(bcrypt.compareSync(password,user.password)){
+    if (bcrypt.compareSync(password, user.password)) {
       const token = uuid()
-      console.log("no token",token)
+      console.log("no token", token)
       await db.collection('sessions').insertOne({
         token,
         userId: user._id
       })
       console.log("pos session")
-      return res.send({token, userId:user._id})
+      return res.send({ token, userId: user._id })
     }
     return res.sendStatus(200)
   } catch (error) {
@@ -73,25 +73,52 @@ app.post('/api/sign-in', async(req, res) => {
   }
 })
 
-app.get('/api/statement', async(req,res) => {
-  const {authorization} = req.headers;
-  const token = authorization?.replace('Bearer','').trim()
+app.get('/api/statement', async (req, res) => {
+  const { authorization } = req.headers;
+  const token = authorization?.replace('Bearer', '').trim()
 
-  if(!token) return res.status(401).send('No session')
+  if (!token) return res.status(401).send('No session')
   try {
-      const session = await db.collection('sessions').findOne({token})
-      if(!session) return res.status(401).send('No active session')
+    const session = await db.collection('sessions').findOne({ token })
+    if (!session) return res.status(401).send('No active session')
 
-      const user = await db.collection('users').findOne({_id:session.userId})
-      if(!user) return res.status(401).send('No user')
+    const user = await db.collection('users').findOne({ _id: session.userId })
+    if (!user) return res.status(401).send('No user')
 
-      const statement = await db.collection('statements').find({userId: user._id}).toArray()
-      console.log("statement aqui")
-      return res.send(statement)
+    const statement = await db.collection('statements').find({ userId: user._id }).toArray()
+    console.log("statement aqui")
+    return res.send(statement)
 
   } catch (error) {
     return res.sendStatus(500)
   }
+})
+
+app.post('/api/statement', async (req, res) => {
+  const { authorization } = req.headers
+  const {type, description, value} = req.body
+  const token = authorization?.replace('Bearer', '').trim()
+  if (!token) return res.status(401).send("No token.")
+
+  try {
+    const session = await db.collection('sessions').findOne({ token })
+    if (!session) return res.status(401).send('No active session')
+
+    const user = await db.collection('users').findOne({ _id: session.userId })
+    if (!user) return res.status(401).send('No user')
+
+    await db.collection('statements').insertOne({
+      type,
+      value,
+      description,
+      userId: user._id
+    })
+    return res.sendStatus(201)
+  } catch (error) {
+    return res.sendStatus(500)
+  }
+
+
 })
 
 const port = 5000;
